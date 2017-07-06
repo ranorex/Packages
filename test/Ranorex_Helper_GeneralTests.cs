411929d9a4b5ec78360ada9261e57ce7ab7fc0f1
Dietmar Haring <dharing@ranorex.com>
2017-07-06 14:28:03 +0200

    Add skeleton for unit test solution

---
+++ test/Ranorex_Helper_GeneralTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ranorex.AutomationHelpers.Tests
+{
+    [TestClass()]
+    public class Ranorex_Helper_GeneralTests
+    {
+        [TestMethod()]
+        public void CompareValuesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
