c92ee76045a01b7e7b9709b057a07f972ea7371b
Lukas Koc <LKoc@ranorex.com>
2017-07-14 16:22:57 +0200

    UCC: Add PopupWatcherLibrary and SystemLibrary (from R_H_General)

---
+++ AutomationHelpers/UserCodeCollections/PopupWatcherLibrary.cs
@@ -0,0 +1,41 @@
+﻿// TODO: Add Header
+
+using Ranorex.Core.Repository;
+using Ranorex.Core.Testing;
+
+namespace Ranorex.AutomationHelpers.UserCodeCollections
+{
+    /// <summary>
+    /// A collection of popup watcher helper methods.
+    /// </summary>
+    [UserCodeCollection]
+    public class PopupWatcherLibrary
+    {
+        /// <summary>
+        /// Waits for a popup window to appear and clicks an element to close the window.
+        /// </summary>
+        /// <param name="findElement">Element to be waited for.</param>
+        /// <param name="clickElement">Elment which should be clicked after the popup appeard.</param>
+        /// <returns>Refference to a newly created <see cref="PopupWatcher"/>></returns>
+        [UserCodeMethod]
+        public PopupWatcher CreatePopupWatcher(RepoItemInfo findElement, RepoItemInfo clickElement)
+        {
+            var watcher = new PopupWatcher();
+            watcher.WatchAndClick(findElement, clickElement);
+            watcher.Start();
+            Report.Info("Popup watcher started.");
+            return watcher;
+        }
+
+        /// <summary>
+        /// Stops the popup watcher.
+        /// </summary>
+        /// <param name="watcher">A watcher to be stopped.</param>
+        [UserCodeMethod]
+        public static void StopPopupWatcher(PopupWatcher watcher)
+        {
+            watcher.Stop();
+            Report.Info("Popup watcher stopped.");
+        }
+    }
+}
---
+++ AutomationHelpers/UserCodeCollections/SystemLibrary.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Ranorex.Core.Testing;
+
+namespace Ranorex.AutomationHelpers.UserCodeCollections
+{
+    /// <summary>
+    /// A collection of usefull general helper methods.
+    /// </summary>
+    [UserCodeCollection]
+    public class SystemLibrary
+    {
+        private static readonly Dictionary<string, System.DateTime> timers = new Dictionary<string, System.DateTime>();
+
+        /// <summary>
+        /// Kills a process
+        /// </summary>
+        /// <param name="processname">Name of the process to kill</param>
+        [UserCodeMethod]
+        public static void KillProcess(string processname)
+        {
+            Process[] processes = Process.GetProcessesByName(processname);
+
+            foreach (Process p in processes)
+            {
+                try
+                {
+                    p.Kill();
+                    Report.Info("Process killed: " + p.ProcessName);
+                }
+                catch (Exception ex)
+                {
+                    Report.Error(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Starts a new timer.
+        /// </summary>
+        /// <param name="timerName">Name of the timer</param>
+        [UserCodeMethod]
+        public static void StartTimer(string timerName)
+        {
+            if (timers.ContainsKey(timerName))
+            {
+                timers.Remove(timerName);
+            }
+
+            timers.Add(timerName, System.DateTime.Now);
+            Report.Log(ReportLevel.Info, "Timer", "Started: '" + timerName + "'");
+        }
+
+        /// <summary>
+        /// Stops a timer.
+        /// </summary>
+        /// <param name="timerName">Name of the timer to stop.</param>
+        [UserCodeMethod]
+        public static TimeSpan StopTimer(string timerName)
+        {
+            if (timers.ContainsKey(timerName))
+            {
+                System.DateTime end = System.DateTime.Now;
+                System.DateTime start = timers[timerName];
+                TimeSpan duration = end - start;
+
+                timers.Remove(timerName);
+
+                Report.Log(
+                    level: ReportLevel.Info,
+                    category: "Timer",
+                    message: "Stopped: '" + timerName + "' (duration: " + (duration.TotalMilliseconds / 1000) + " seconds)");
+
+                return duration;
+            }
+
+            throw new Exception("Timer '" + timerName + "' does not exist.");
+        }
+    }
+}
