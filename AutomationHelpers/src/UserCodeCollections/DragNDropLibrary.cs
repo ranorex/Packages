28014bea223868090958922f99f3e9c4912914f7
Dietmar Haring <dharing@ranorex.com>
2017-11-03 12:31:57 +0100

    Helpers: Minor refactorings
    No functional changes indented!
     - Removed unused usings,
     - changed copyright to default,
     - added static to UserCodeCollection classes and
     - replaced tabs with spaces.

--- AutomationHelpers/src/UserCodeCollections/DragNDropLibrary.cs
+++ AutomationHelpers/src/UserCodeCollections/DragNDropLibrary.cs
@@ -1,34 +1,26 @@
-﻿/*
- * Created by Ranorex
- * User: sknopper
- * Date: 9/19/2017
- * Time: 12:22 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+﻿//
+// Copyright © 2017 Ranorex All rights reserved
+//
+
 using System.Drawing;
-
-using Ranorex;
-
 using Ranorex.Core.Testing;
 
-namespace CheckAutomationHelperIntegration.UserCodeCollections
+namespace Ranorex.AutomationHelpers.UserCodeCollections
 {
     /// <summary>
-    /// Ranorex User Code collection. A collection is used to publish User Code methods to the User Code library.
+    /// A collection providing an advanced DragAndDrop method.
     /// </summary>
     [UserCodeCollection]
-    public class DragNDropLibrary
+    public static class DragNDropLibrary
     {
-
         /// <summary>
         /// Picks the source element, drags it to the target element and drops it there.
         /// <param name="source">The element which should be dragged.</param>
         /// <param name="target">The element where the source elemente will be dropped.</param>
         /// </summary>
         [UserCodeMethod]
-    	public static void DragAndDrop(Adapter source, Adapter target)
-    	{
+        public static void DragAndDrop(Adapter source, Adapter target)
+        {
             source.Focus();
             source.MoveTo();
             Mouse.ButtonDown(System.Windows.Forms.MouseButtons.Left);
