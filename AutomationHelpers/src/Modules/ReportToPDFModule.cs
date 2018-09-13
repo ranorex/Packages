addd8f41c6563ed6abe927f283c41c44217401db
Didddy <dharing@ranorex.com>
2018-09-13 14:26:38 +0200

    Helpers: Add property to specify .pdf file output directory (#35)

--- AutomationHelpers/src/Modules/ReportToPDFModule.cs
+++ AutomationHelpers/src/Modules/ReportToPDFModule.cs
@@ -39,6 +39,9 @@ namespace Ranorex.AutomationHelpers.Modules
         [TestVariable("1d09c6b5-db7a-4ed0-a13c-ee2add5d3d37")]
         public string PdfName { get; set; }
 
+        [TestVariable("536f8a69-f246-4f2b-ae9e-1f62babb2ba9")]
+        public string PdfPath { get; set; }
+
         [TestVariable("c15f25ea-9409-4e1a-b76e-cd208bae6c56")]
         public string Xml { get; set; }
 
@@ -98,7 +101,7 @@ namespace Ranorex.AutomationHelpers.Modules
 
             try
             {
-                pdfReportFilePath = ConvertReportToPDF(this.PdfName, this.Xml, this.Details);
+                pdfReportFilePath = ConvertReportToPDF(this.PdfName, this.PdfPath, this.Xml, this.Details);
                 Report.LogHtml(
                     ReportLevel.Success,
                     "PDFReport",
@@ -123,10 +126,12 @@ namespace Ranorex.AutomationHelpers.Modules
             return pdfReportFilePath;
         }
 
-        private string ConvertReportToPDF(string pdfName, string xml, string details)
+        private string ConvertReportToPDF(string pdfName, string pdfPath, string xml, string details)
         {
-            var zippedReportFileDirectory = CreateTempReportFileDirectory();
-            var reportFileDirectory = TestReport.ReportEnvironment.ReportFileDirectory;
+        	var zippedReportFileDirectory = CreateTempReportFileDirectory();
+            var reportFileDirectory = string.IsNullOrEmpty(pdfPath)
+                ? TestReport.ReportEnvironment.ReportFileDirectory
+                : pdfPath;
             var name = TestReport.ReportEnvironment.ReportName;
 
             var input = String.Format(@"{0}\{1}.rxzlog", zippedReportFileDirectory, name);
